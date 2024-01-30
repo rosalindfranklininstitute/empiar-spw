8f56cb0ac69533e5f8f9ac665159770247461b6e
sriram <sriram@ebi.ac.uk>
2024-01-30 20:28:16 +0000

    corrections on home tsx rendering

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,37 +2,46 @@ import HomeTiles from "../components/HomeTiles";
 import WorkflowVisualisationCard from "../components/WorkFlowVisualisationCard";
 import "../widget/Widget.css";
 import Stack from '@mui/material/Stack';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 
 
 
 function Home() {
   return (
     <>
-      <Stack direction="row" spacing={3}>
-        <HomeTiles
-          title="Published Entries"
-          descritpion="Entries that are submitted and made public."
-          route="published"
-          entrycount={4}></HomeTiles>
-        <HomeTiles
-          title="Template Entries"
-          descritpion="Template entries from which users can create a new workflow"
-          route="template"
-          entrycount={4}></HomeTiles>
-      </Stack>
-      <Stack direction="row" spacing={3}>
-        <HomeTiles
-          title="Saved Entries"
-          descritpion="Saved workflows "
-          route="saved"
-          entrycount={4}></HomeTiles>
-        <HomeTiles
-          title="Build A Workflow"
-          descritpion="Create a new workflow"
-          route="build"
-          entrycount={4}></HomeTiles>
-      </Stack>
-      <h4>Home</h4>
+      <CssBaseline />
+      <Container maxWidth="md">
+        <Stack direction="column" spacing={2}>
+          <h2>Sample Preparation Widget (SPW)</h2>
+          <Typography>Sample Preparation Widget (SPW) is an online tool available through the EMPIAR deposition system. SPW is an easy-to-use and re-usable web component that allows for the detailed and structured description of Sample Preparation (SP) protocols used for volume EM (vEM) experiments. The widget (working with a Mongo database) provides a repository of SP data that can be shared with other users and used for further analysis.</Typography>
+          <Stack direction="row" spacing={5} sx={{padding: "0px 20px"}}>
+            <HomeTiles
+              title="Published Entries"
+              descritpion="Entries that are submitted and made public."
+              route="published"
+              entrycount={4}></HomeTiles>
+            <HomeTiles
+              title="Template Entries"
+              descritpion="Template entries from which users can create a new workflow"
+              route="template"
+              entrycount={4}></HomeTiles>
+          </Stack>
+          <Stack direction="row" spacing={5} sx={{padding: "0px 20px"}}>
+            <HomeTiles
+              title="Saved Entries"
+              descritpion="Saved workflows "
+              route="saved"
+              entrycount={4}></HomeTiles>
+            <HomeTiles
+              title="Build A Workflow"
+              descritpion="Create a new workflow"
+              route="build"
+              entrycount={4}></HomeTiles>
+          </Stack>
+        </Stack>
+      </Container>
     </>
   );
 }
