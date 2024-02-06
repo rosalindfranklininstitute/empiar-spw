ce33f5ddd9103059e14d99bc117bba699eebd884
sriram <sriram@ebi.ac.uk>
2024-02-06 19:42:15 +0000

    initial concurrent set up of local api complete

---
+++ server/index.ts
@@ -0,0 +1,16 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import saved from "../data/localapi/saved.json";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 5000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
